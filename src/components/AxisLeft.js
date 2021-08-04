0da1cc6a79e265a3b8413b6acc69d7dec7b93a2a
Jake <jake.a.reddy@gmail.com>
2021-08-04 22:18:37 +0300

    fixed warning in AxisLeft, no key

--- src/components/AxisLeft.js
+++ src/components/AxisLeft.js
@@ -4,9 +4,8 @@ export const AxisLeft = ({ yScale }) => {
       {yScale.ticks().map((tickValue) => (
         //Now we take the yScale and access the domain object(which is the country we passed in earlier line 36) and we map them and extract the tickvalue
         //we can use the yScale(tickvalue) to get the starting point of each bar and offset it by  half of the yScale.bandwidth to center the text in each bar
-        <g className='tick'>
+        <g className='tick' key={tickValue}>
           <text
-            key={tickValue}
             style={{ textAnchor: 'end' }}
             dy='.32em'
             x='-3'
