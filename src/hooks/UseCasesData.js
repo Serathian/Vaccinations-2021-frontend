0e47b081f06517ab8b069ca16947cbdf6ad49d6c
Jake <jake.a.reddy@gmail.com>
2021-08-06 00:25:45 +0300

    removed axios code

--- src/hooks/UseCasesData.js
+++ src/hooks/UseCasesData.js
@@ -1,37 +1,5 @@
 import { useEffect, useState } from 'react'
-import localforage from 'localforage'
-import memoryDriver from 'localforage-memoryStorageDriver'
-import { setup } from 'axios-cache-adapter'
 import { csv } from 'd3'
-
-const configure = async () => {
-  // Register the custom `memoryDriver` to `localforage`
-  await localforage.defineDriver(memoryDriver)
-
-  // Create `localforage` instance
-  const forageStore = localforage.createInstance({
-    // List of drivers used
-    driver: [
-      localforage.INDEXEDDB,
-      localforage.LOCALSTORAGE,
-      memoryDriver._driver,
-    ],
-    // Prefix all storage keys to prevent conflicts
-    name: 'cases',
-  })
-
-  // Create `axios` instance with pre-configured `axios-cache-adapter` using a `localforage` store
-  return setup({
-    // `axios` options
-    baseURL: 'http://localhost:4000/api',
-
-    // `axios-cache-adapter` options
-    cache: {
-      maxAge: 15 * 60 * 1000,
-      store: forageStore, // Pass `localforage` store to `axios-cache-adapter`
-    },
-  })
-}
 
 const csvUrl =
   'https://gist.githubusercontent.com/Serathian/86c591b116df5efe282a8350f4685e61/raw/938ea4e230d56faa68b94fd5a3cc35ee4881b79f/'
@@ -47,12 +15,3 @@ export const UseCasesData = () => {
   }, [])
   return data
 }
-
-// useEffect(() => {
-//   configure().then(async (api) => {
-//     const response = await api.get('/cases')
-//     const data = response.data
-
-//     setData(data)
-//   })
-// }, [])
