44cc2110f7abbc5f038783f14baec14954bdcd76
Jake <jake.a.reddy@gmail.com>
2021-08-06 18:52:02 +0300

    added filtering and extended view

--- src/pages/VaccinationsWithOverlay.js
+++ src/pages/VaccinationsWithOverlay.js
@@ -9,7 +9,7 @@ import { Lines } from '../components/Lines'
 
 import { getDaysArray, getYScaleDomain } from '../utils/graph-helpers'
 
-//TODO: Add filtering and clean code
+//TODO: Add filtering drop down boxes
 
 //Variables for the dimensions of the graph
 const width = 960
@@ -19,13 +19,37 @@ const innerHeight = height - margin.top - margin.bottom
 const innerWidth = width - margin.left - margin.right
 
 const VaccinationsWithOverlay = ({ vaccinationsData, casesData }) => {
-  const [extendedView, setExtendedView] = useState(7)
+  const [extendedView, setExtendedView] = useState(true)
+  const handleExtendedView = () => {
+    setExtendedView(!extendedView)
+  }
+
   const [dateValueMax, setDateValueMax] = useState(new Date())
   const [dateValueExtended, setDateValueExtended] = useState(new Date())
   const [dateValueMin, setDateValueMin] = useState(new Date())
 
-  const [vaccinationsFilter, setVaccinationsFilter] = useState('')
-  const [casesFilter, setCasesFilter] = useState('All areas')
+  const [filterSelector, setFilterSelector] = useState(0)
+  const vaccinationsFilterOptions = ['', 'HYKS', 'KYS', 'OYS', 'TAYS', 'TYKS']
+  const [vaccinationsFilter, setVaccinationsFilter] = useState(
+    vaccinationsFilterOptions[0]
+  )
+  const casesFilterOptions = [
+    'All areas',
+    'Helsinki and Uusimaa Hospital District',
+    'North Savo Hospital District',
+    'North Ostrobothnia Hospital District',
+    'Pirkanmaa Hospital District',
+    'Southwest Finland Hospital District',
+  ]
+  const [casesFilter, setCasesFilter] = useState(casesFilterOptions[0])
+
+  const handleSelectorChange = (e) => {
+    console.log(e.target.value)
+    const index = e.target.value
+    setFilterSelector(index)
+    setVaccinationsFilter(vaccinationsFilterOptions[index])
+    setCasesFilter(casesFilterOptions[index])
+  }
 
   useEffect(() => {
     if (vaccinationsData) {
@@ -36,12 +60,19 @@ const VaccinationsWithOverlay = ({ vaccinationsData, casesData }) => {
         new Date(d3.max(vaccinationsData, (d) => d.vaccinationDate))
       )
     }
-  }, [extendedView, vaccinationsData])
+  }, [vaccinationsData])
 
   useEffect(() => {
-    setDateValueExtended(
-      dateValueMax.setDate(dateValueMax.getDate() + extendedView)
-    )
+    if (extendedView) {
+      setDateValueExtended(
+        new Date(dateValueMax).setDate(dateValueMax.getDate() + 15)
+      )
+    }
+    if (!extendedView) {
+      setDateValueExtended(
+        new Date(dateValueMax).setDate(dateValueMax.getDate())
+      )
+    }
   }, [dateValueMax, extendedView])
 
   //Null check data is received
@@ -50,6 +81,7 @@ const VaccinationsWithOverlay = ({ vaccinationsData, casesData }) => {
   }
 
   //#region Data Parsing Logic
+
   //-- Data filtering
   const filteredVaccinationsData = !vaccinationsFilter
     ? vaccinationsData
@@ -57,12 +89,11 @@ const VaccinationsWithOverlay = ({ vaccinationsData, casesData }) => {
         (obj) => obj.sourceBottle.healthCareDistrict === vaccinationsFilter
       )
 
-  // console.log(filteredVaccinationsData[0])
   const filteredCasesData = casesData.filter(
     (obj) =>
       obj.Area === casesFilter &&
       obj.Time > dateValueMin &&
-      obj.Time < dateValueMax
+      obj.Time < dateValueExtended
   )
 
   //-- Getting thresholds for the binning
@@ -75,9 +106,11 @@ const VaccinationsWithOverlay = ({ vaccinationsData, casesData }) => {
     .thresholds(threshold)(filteredVaccinationsData)
   //#endregion
 
+  //Null check if data if parsing
   if (!filteredVaccinationsData || !filteredCasesData || !vaccinationBuckets) {
     return <pre>Parsing Data...</pre>
   }
+
   //#region Visualization Logic
   //-- xAxis values
   const xValueCases = (d) => d.Time
@@ -86,6 +119,7 @@ const VaccinationsWithOverlay = ({ vaccinationsData, casesData }) => {
     .scaleTime()
     .domain([dateValueMin, dateValueExtended])
     .range([0, innerWidth])
+    .nice()
 
   //-- yAxis Values
   const yValueVaccine = (d) => d.length
@@ -103,28 +137,76 @@ const VaccinationsWithOverlay = ({ vaccinationsData, casesData }) => {
     .domain([yScaleDomain, 0])
     .range([0, innerHeight])
     .nice()
+  //#endregion
 
   return (
-    <svg width={width} height={height}>
-      <g transform={`translate(${margin.left}, ${margin.top})`}>
-        <AxisBottom xScale={xScale} innerHeight={innerHeight} />
-        <AxisLeft yScale={yScale} innerWidth={innerWidth} />
-        <Marks
-          data={vaccinationBuckets}
-          yScale={yScale}
-          xScale={xScale}
-          yValue={yValueVaccine}
-          innerHeight={innerHeight}
-        />
-        <Lines
-          data={filteredCasesData}
-          yScale={yScale}
-          xScale={xScale}
-          yValue={yValueCases}
-          xValue={xValueCases}
-        />
-      </g>
-    </svg>
+    <div id='graph'>
+      <h1>Correlation of COVID-19 vaccinations and Reported cases</h1>
+      <h5>
+        This is a graphical representation of COVID-19 vaccinations (Source:
+        {
+          <a href='https://github.com/solita/vaccine-exercise-2021'>
+            Solita Exercise Git Repo
+          </a>
+        }
+        ) compared with Daily reported COVID-19 cases (Source:{' '}
+        {
+          <a href='https://sampo.thl.fi/pivot/prod/en/epirapo/covid19case'>
+            sampo.thl.fi
+          </a>
+        }
+        )
+      </h5>
+      <ul>
+        <li>
+          <strong>Area</strong> - The provence displayed
+        </li>
+        <li>
+          <strong>Extended View</strong> - Toggle drop-off period (2 weeks / 15
+          days)
+        </li>
+      </ul>
+      <label>
+        Area:
+        <select
+          id='area'
+          value={filterSelector}
+          onChange={(e) => handleSelectorChange(e)}>
+          <option value={0}>All Areas</option>
+          <option value={1}>Uusimaa</option>
+          <option value={2}>North Savo</option>
+          <option value={3}>North Ostrobothnia</option>
+          <option value={4}>Pirkanmaa</option>
+          <option value={5}>Southwest Finland</option>
+        </select>
+      </label>
+      <label>
+        Extended View:
+        <button id='extended-view' onClick={handleExtendedView}>
+          {extendedView ? 'Hide' : 'Show'}
+        </button>
+      </label>
+      <svg width={width} height={height}>
+        <g transform={`translate(${margin.left}, ${margin.top})`}>
+          <AxisBottom xScale={xScale} innerHeight={innerHeight} />
+          <AxisLeft yScale={yScale} innerWidth={innerWidth} />
+          <Marks
+            data={vaccinationBuckets}
+            yScale={yScale}
+            xScale={xScale}
+            yValue={yValueVaccine}
+            innerHeight={innerHeight}
+          />
+          <Lines
+            data={filteredCasesData}
+            yScale={yScale}
+            xScale={xScale}
+            yValue={yValueCases}
+            xValue={xValueCases}
+          />
+        </g>
+      </svg>
+    </div>
   )
 }
 
